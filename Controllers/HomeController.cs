822be13e4c50198b428bf59025f06f7a66f7ae54
salamha <alakwaass@gmail.com>
2017-04-29 15:09:33 -0700

    SMA-29-04-2017
    FRIST VER1 IN 29 - 04 - 2017 FOR TEST GITHUB

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace SMA.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult index1()
+        {
+           
+
+            return View();
+        }
+
+        public ActionResult indexAdminAR()
+        {
+            
+
+            return View();
+        }
+    }
+}
